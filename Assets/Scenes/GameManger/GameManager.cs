d6e7180f017809b766d86a8b07bf0cc88b3a9631
Rodrigo Pintos <r.pintos.banchero@gmail.com>
2023-04-09 20:15:09 -0300

    player movement almost done

---
+++ Assets/Scenes/GameManger/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public float playerVelocity = 1.0f;
+    [SerializeField]
+    [Range(0, 5)]
+    public float maximumPlayerDisplacement = 0;
+
+    public float enemiesVelocity = 1.0f;
+    public float enemiesVelocityMultiplier = 1;
+
+    //horizontalMovement is a float between -1,1
+    public void changePlayerVelocity(float horizontalMovement)
+    {
+        enemiesVelocityMultiplier += horizontalMovement;
+    }
+}
+
--- Assets/Scenes/PlayerManager/PlayerMovementController.cs
+++ Assets/Scenes/PlayerManager/PlayerMovementController.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    private float initialXPosition;
+
+    private void Start()
+    {
+        initialXPosition = transform.position.x;
+    }
 
     private void Update()
     {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
+
+        var playerVelocity = GameManager.Instance.playerVelocity;
+
+        var deltaY = y * playerVelocity * Time.deltaTime;
+        transform.position += new Vector3(0, deltaY, 0);
+
+        var currentX = transform.position.x;
+        var deltaX = x * playerVelocity * Time.deltaTime;
+        var newX = currentX + deltaX;
+
+        var finalX = Mathf.Clamp(
+            newX,
+            initialXPosition - GameManager.Instance.maximumPlayerDisplacement,
+            initialXPosition + GameManager.Instance.maximumPlayerDisplacement
+        );
+        
+        transform.position = new Vector3(finalX, transform.position.y, 0);
+
+        GameManager.Instance.changePlayerVelocity(x*Time.deltaTime);
     }
 
 }
