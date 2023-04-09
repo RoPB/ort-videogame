ad6aae75c528d2b5b4674c31105ca23195fc3437
Rodrigo Pintos <r.pintos.banchero@gmail.com>
2023-04-09 19:24:09 -0300

    using player and enemy types instead of tags

---
+++ Assets/Scenes/PlayerManager/PlayerMovementController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    private void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+    }
+
+}
+
