83590473e67233a6fe975fba549269ea6653ff73
Rodrigo Pintos <r.pintos.banchero@gmail.com>
2023-04-11 21:34:41 -0300

    move logic to gameManager

--- Assets/Scenes/GameManger/GameManager.cs
+++ Assets/Scenes/GameManger/GameManager.cs
@@ -12,6 +12,16 @@ public struct SceneBounds{
 public class GameManager : MonoBehaviour
 {
 
+    private SceneBounds sceneBounds;
+
+    public float playerVelocity = 1.0f;
+    [SerializeField]
+    [Range(0, 1)]
+    public float maximumPlayerDisplacement = 0;
+
+    public float enemiesVelocity = 1.0f;
+    public float enemiesVelocityMultiplier = 1;
+
     public static GameManager Instance { get; private set; }
     private void Awake()
     {
@@ -23,34 +33,66 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            Instance = this;
-            Instance.sceneBounds = new SceneBounds()
+            this.sceneBounds = new SceneBounds()
             {
                 topRightCorner = this.sceneTopRightCorner(),
                 bottomRightCorner = this.sceneBottomRightCorner(),
                 topLeftCorner = this.sceneTopLeftCorner(),
                 bottomLeftCorner = this.sceneBottomLeftCorner()
             };
+            Instance = this;
         }
     }
-
-    public SceneBounds sceneBounds;
-
-    public float playerVelocity = 1.0f;
-    [SerializeField]
-    [Range(0, 5)]
-    public float maximumPlayerDisplacement = 0;
-
-    public float enemiesVelocity = 1.0f;
-    public float enemiesVelocityMultiplier = 1;
 
     //horizontalMovement is a float between -1,1
     public void changePlayerVelocity(float horizontalMovement)
     {
-        enemiesVelocityMultiplier += horizontalMovement;
+        enemiesVelocityMultiplier = 1 + horizontalMovement;
     }
 
-    #region auxiliar
+    public bool isAtTheLeftOfTheScene(Vector3 transform, Vector3 localScale)
+    {
+        return transform.x + localScale.x / 2 < GameManager.Instance.sceneBounds.topLeftCorner.x;
+            //|| transform.x + localScale.x / 2 > GameManager.Instance.sceneBounds.topRightCorner.x
+            //|| transform.y + localScale.y / 2 > GameManager.Instance.sceneBounds.topLeftCorner.y
+            //|| transform.y + localScale.y / 2 < GameManager.Instance.sceneBounds.bottomLeftCorner.y;
+    }
+
+    public float clampXInSceneBounds(float newX, float width)
+    {
+        return Math.Clamp(
+                    newX,
+                    GameManager.Instance.sceneBounds.bottomLeftCorner.x + width / 2,
+                    GameManager.Instance.sceneBounds.bottomRightCorner.x - width / 2
+                );
+    }
+
+    public float clampYInSceneBounds(float newY, float height)
+    {
+        return Math.Clamp(
+            newY,
+            GameManager.Instance.sceneBounds.bottomLeftCorner.y + height / 2,
+            GameManager.Instance.sceneBounds.topLeftCorner.y - height / 2
+        );
+    }
+
+    public float getRandomYInSceneBounds()
+    {
+        var sceneTopRightCorner = GameManager.Instance.sceneBounds.topRightCorner;
+        var sceneBottomRightCorner = GameManager.Instance.sceneBounds.bottomRightCorner;
+        var maxY = sceneTopRightCorner.y - transform.localScale.y / 2;
+        var minY = sceneBottomRightCorner.y + transform.localScale.y / 2;
+        System.Random random = new System.Random();
+        double randomY = (random.NextDouble() * (maxY - minY) + minY);
+        return (float)randomY;
+    }
+
+    public float getSceneMaxX()
+    {
+        return this.sceneBounds.topRightCorner.x;
+    }
+
+    #region Bounds Auxiliar Methods
 
     private Vector3 sceneTopRightCorner()
     {
