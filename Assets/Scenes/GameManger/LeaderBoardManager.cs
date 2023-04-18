7fdcbf3139ae332b371f2e2ee9a9ae37874d1ce5
Juan Casanova <j.casanoval@hotmail.com>
2023-04-18 20:45:51 -0300

    Updated way to retrieve leaderboard data

--- Assets/Scenes/GameManger/LeaderBoardManager.cs
+++ Assets/Scenes/GameManger/LeaderBoardManager.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Dan.Main;
+using TMPro;
 
 public class LeaderBoardManager : MonoBehaviour
 {
     private const string LeaderboardPublicKey = "fca0e5a15240f0341538061fd6129ce2a1f9918031ee0c8fbb84ad5bb397421d";
     private const string LeaderboardSecret = "1621928aad8305387fe2b9c451a2a62831ab548336fe30158049df396731446516f63d49c5b5e6a57491dfb80864d2278bb6748e12908032c160f920f290351cfdd003a3077dcf196b6880cafeb6fb191fa0f450678cb062a9fda9e8498fde74ad2439b580c4317fa9592cd84275cf489e6dc5218b393251f3e7a027ef40c2b5";
-
-    public List<LeaderboardEntry> leaderboardEntries = new List<LeaderboardEntry>();
 
     public void SubmitScoreAsync(string name, int score)
     {
@@ -19,31 +18,34 @@ public class LeaderBoardManager : MonoBehaviour
         });
     }
 
-    public void GetScoresAsync()
+    public void GetScoresAsync(List<TextMeshProUGUI> names, List<TextMeshProUGUI> scores)
     {
         Debug.Log("GetScoresAsync");
         var query = new Dan.Models.LeaderboardSearchQuery()
         {
-            Take = 10,
+            Take = Mathf.Min(names.Count, scores.Count),
         };
+
         LeaderboardCreator.GetLeaderboard(LeaderboardPublicKey, query, (leaderboard) =>
         {
-            foreach (var entry in leaderboard)
+            for (int i = 0; i < leaderboard.Length; i++)
             {
-                Debug.Log(entry.Username + " " + entry.Score);
-                var e = new LeaderboardEntry()
+                var entry = leaderboard[i];
+                names[i].text = entry.Username;
+                scores[i].text = entry.Score.ToString();
+                if (entry.IsMine())
                 {
-                    name = entry.Username,
-                    score = entry.Score,
-                };
-                leaderboardEntries.Add(e);
+                    names[i].color = Color.yellow;
+                    scores[i].color = Color.yellow;
+                }
+                else
+                {
+                    names[i].color = Color.white;
+                    scores[i].color = Color.white;
+                }
             }
         });
     }
-}
 
-public struct LeaderboardEntry
-{
-    public string name;
-    public int score;
+
 }
