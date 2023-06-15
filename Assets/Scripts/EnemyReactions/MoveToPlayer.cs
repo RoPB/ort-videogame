17630f9608609128406bcd06bca844643d068ad6
Rodrigo Pintos <r.pintos.banchero@gmail.com>
2023-06-15 00:43:47 -0300

    velocity movesss

--- Assets/Scripts/EnemyReactions/MoveToPlayer.cs
+++ Assets/Scripts/EnemyReactions/MoveToPlayer.cs
@@ -17,10 +17,10 @@ public class MoveToPlayer : IReaction
         //_rigidbody.velocity = new Vector2(0, 0);
         //_rigidbody.bodyType = RigidbodyType2D.Static;
         _rigidbody.bodyType = RigidbodyType2D.Kinematic;
-        _rigidbody.velocity = new Vector2(0, 0);
-        //_rigidbody.velocity = Vector2.up * 1000f;
+        //_rigidbody.velocity = new Vector2(0, 0);
+        _rigidbody.velocity = Vector2.up * 1;
         //_rigidbody.AddForce(Vector2.up * 1000f, ForceMode2D.Impulse);
-        _rigidbody.AddForce(Vector2.up*10f, ForceMode2D.Impulse);
+        //_rigidbody.AddForce(Vector2.up*10f, ForceMode2D.Impulse);
     }
 }
 
