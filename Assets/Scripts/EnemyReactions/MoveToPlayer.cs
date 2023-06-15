3b39695a5a1b1c897cee815c8bb6dbd6face33c5
Rodrigo Pintos <r.pintos.banchero@gmail.com>
2023-06-15 20:36:55 -0300

    moving to player

--- Assets/Scripts/EnemyReactions/MoveToPlayer.cs
+++ Assets/Scripts/EnemyReactions/MoveToPlayer.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class MoveToPlayer : IReaction
 {
     private Rigidbody2D _rigidbody;
+    private Vector2 _forceDirection;
+    private int _movePlayerCounter = 0;
 
     void Start()
     {
         _rigidbody = this.gameObject.GetComponentInParent<Rigidbody2D>();
     }
 
+    void FixedUpdate()
+    {
+        if (_movePlayerCounter>0)
+        {
+            --_movePlayerCounter;
+            _rigidbody.velocity = Vector2.zero;
+            _rigidbody.AddForce(_forceDirection * 1f, ForceMode2D.Impulse);
+        }
+        
+    }
+
 
     public override void React(Collider2D collision)
     {
-        Debug.Log("COLISIONA");
-        //_rigidbody.velocity = new Vector2(0, 0);
-        //_rigidbody.bodyType = RigidbodyType2D.Static;
-        _rigidbody.bodyType = RigidbodyType2D.Kinematic;
-        //_rigidbody.velocity = new Vector2(0, 0);
-        _rigidbody.velocity = Vector2.up * 1;
-        //_rigidbody.AddForce(Vector2.up * 1000f, ForceMode2D.Impulse);
-        //_rigidbody.AddForce(Vector2.up*10f, ForceMode2D.Impulse);
+        _movePlayerCounter = 10;
+        Vector2 forceDirection = collision.transform.position - transform.position;
+        forceDirection.Normalize();
+        _forceDirection = forceDirection;
+        
     }
 }
 
