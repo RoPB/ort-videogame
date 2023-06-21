bd20f786887d5fea223de6231e8722be9be0e99a
Juan Casanova <j.casanoval@hotmail.com>
2023-06-20 21:21:35 -0300

    Added player movement with rigidBody - Added bounds - Added flame effects

--- Assets/Managers/PlayerManager/PlayerManager.cs
+++ Assets/Managers/PlayerManager/PlayerManager.cs
@@ -9,14 +9,13 @@ public class PlayerManager : MonoBehaviour
 
     private Player _player => GameObject.FindObjectOfType<Player>();
 
-    public float playerHeight  { get { return _player.playerHeight; } }
+    public float playerHeight { get { return _player.playerHeight; } }
 
     public float playerWidth { get { return _player.playerWidth; } }
 
     public void Init(string playerName)
     {
         _playerName = playerName;
-        _player.playerMovementController.Init();
     }
 }
 
