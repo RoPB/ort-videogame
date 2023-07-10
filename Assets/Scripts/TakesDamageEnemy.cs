35d23ff7690de395023b4979ed8f7a0d3dde4b88
Juan Casanova <j.casanoval@hotmail.com>
2023-07-09 22:41:22 -0300

    Remove inmortality period for enemies

--- Assets/Scripts/TakesDamageEnemy.cs
+++ Assets/Scripts/TakesDamageEnemy.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class TakesDamageEnemy : TakesDamage
 {
-    private float inmortalityPeriod = 0.3f;
-    private float lastDamageTime = 0;
     public override void TakeDamage(Collision2D collision, int damage)
     {
-        if (Time.time - lastDamageTime < inmortalityPeriod)
-            return;
-        lastDamageTime = Time.time;
         this.GetComponent<Enemy>().TookDamage(collision, damage);
     }
 }
